cc6d0303dbfe73d26ffd437746a2f5354ca833e8
Brenda <brenda.leite1@hotmail.com>
2020-06-03 18:57:23 -0300

    #003

---
+++ #003.py
@@ -0,0 +1,6 @@
+#Exercício Python 003: Crie um programa que leia dois números e mostre a soma entre eles.
+number_1 = int(input('Enter a first number: '))
+number_2 = int(input('Enter a second number: '))
+sum_all = number_1 + number_2
+
+print(sum_all)
