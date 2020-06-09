6d0583db873abc9f4f17d8eaa81c046bac099503
Brenda <49522696+bitwoman@users.noreply.github.com>
2020-06-09 19:36:41 -0300

    #019

--- #019.py
+++ #019.py
@@ -1,6 +1,6 @@
 # Exercício Python 019: Um professor quer sortear um dos seus quatro alunos para apagar o quadro. 
 # Faça um programa que ajude ele, lendo o nome dos alunos e escrevendo na tela o nome do escolhido.
-import random
+from random import choice
 name_list = []
 
 for x in range(0,3):
