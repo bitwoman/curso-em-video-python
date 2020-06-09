26348ddf46f7556f72da76d0c6f6ae1b0338b58d
Brenda <49522696+bitwoman@users.noreply.github.com>
2020-06-09 18:05:12 -0300

    #017

--- #017.py
+++ #017.py
@@ -5,6 +5,7 @@ opposite_side = float(input('Enter the value of the opposite side: '))
 adjacent_side = float(input('Enter the value of the adjacent side:'))
 sum = (opposite_side**2) + (adjacent_side**2)
 #or (opposite_side**2) + (adjacent_side**2) * (1/2)
+#or hypot(opposite_side, adjacent_side)
 h = sqrt(sum)
 
 print(f' %.2f' %h)
