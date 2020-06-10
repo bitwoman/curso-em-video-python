527d38e2fb9091207872559cd29b57657facf9ad
Brenda <49522696+bitwoman@users.noreply.github.com>
2020-06-10 20:17:40 -0300

    #027

---
+++ #027.py
@@ -0,0 +1,5 @@
+# Exercício Python 027: Faça um programa que leia o nome completo de uma pessoa, mostrando em seguida o primeiro e o último nome separadamente.
+full_name = str(input('Enter your full name: ')).title().split()
+first_name = full_name[0]
+last_name = full_name[-1]
+print(f'First name: {first_name}\nLast name: {last_name}')
