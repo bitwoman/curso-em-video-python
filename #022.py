14fe7b6a2400055966e42ee0f5d1febeff3bc32a
Brenda <49522696+bitwoman@users.noreply.github.com>
2020-06-10 15:02:06 -0300

    #022

--- #022.py
+++ #022.py
@@ -2,10 +2,10 @@
 # - O nome com todas as letras maiúsculas e minúsculas.
 # - Quantas letras ao todo (sem considerar espaços).
 # - Quantas letras tem o primeiro nome.
-name = str(input('Enter your full name: '))
+name = str(input('Enter your full name: ')) #or name = str(input('Enter your full name: ')).strip()
 
 print(name.upper())
 print(name.lower())
-print(len(name.replace(' ', '')))
+print(len(name.replace(' ', ''))) #or print(len(name) - name.count(' ')
 new_name = name.split()
-print(len(new_name[0]))
+print(len(new_name[0])) #or print(len(new_name.find(' '))
