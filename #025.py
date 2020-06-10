18c6a05774c48952ad8867bef1df6225814f9cfe
Brenda <49522696+bitwoman@users.noreply.github.com>
2020-06-10 16:25:04 -0300

    #025

---
+++ #025.py
@@ -0,0 +1,3 @@
+# Exercício Python 025: Crie um programa que leia o nome de uma pessoa e diga se ela tem "SILVA" no nome.
+name = str(input('Enter your full name: ')).upper().strip()
+'SILVA' in name
