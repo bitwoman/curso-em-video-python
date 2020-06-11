325b92b75bd1542f3d9474b9d047fd1232ef52f5
Brenda <49522696+bitwoman@users.noreply.github.com>
2020-06-11 14:23:04 -0300

    #030

--- #030.py
+++ #030.py
@@ -1,7 +1,7 @@
 # Exercício Python 030: Crie um programa que leia um número inteiro e mostre na tela se ele é PAR ou ÍMPAR.
 number = int(input('Enter any number: '))
 
-if number % 2 == 0: #or if 2*int(n/2) == n
+if number % 2 == 0: #or if 2*int(number/2) == number or if number == int(number):
   print('Even number!')
 else:
   print('Odd number!')
