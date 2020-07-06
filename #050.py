5ea4da111ce4c071ca38e92223865ea0fd964302
Brenda <49522696+bitwoman@users.noreply.github.com>
2020-07-06 19:45:41 -0300

    #050

--- #050.py
+++ #050.py
@@ -1,11 +1,13 @@
 # Exercício Python 050: Desenvolva um programa que leia seis números inteiros e 
 # mostre a soma apenas daqueles que forem pares. Se o valor digitado for ímpar, desconsidere-o.
 sum = 0
+count = 0
 
 for x in range(1,7):
   n = int(input('Enter a number: '))
 
   if n % 2 == 0:
-    sum = sum + n
+    sum += n
+    count += 1
 
-print(sum)
+print(count, sum)
